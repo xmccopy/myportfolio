2bc3c96485a7bf1168d22bafb5d0bf102ce9800c
xmccopy <comacmccopy@gmail.com>
2024-08-27 15:57:18 +0900

    portfolio

---
+++ constants/constants.js
@@ -0,0 +1,67 @@
+export const NAME = 'Muhammad Osama Javaid';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'Pakistan',
+  City: 'Peshawar',
+  Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'iosamajavaid@gmail.com',
+  PHONE: '+923159591822',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/osamajavaid',
+  LINKEDIN: 'https://www.linkedin.com/in/iosamajavaid/',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
