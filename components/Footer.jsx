1a88d248cd064c54e15507823d3ca9102c96b670
xmccopy <comacmccopy@gmail.com>
2024-08-27 16:02:05 +0900

    fix: contact address

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                     <div className="mr-1 text-base">
                         <MdMail />
                     </div>
-                    <div>iosamajavaid@gmail.com</div>
+                    <div>mccopybaica@gmail.com</div>
                 </div>
             </div>
         </div>
